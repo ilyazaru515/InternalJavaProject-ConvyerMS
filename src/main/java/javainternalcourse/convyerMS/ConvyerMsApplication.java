39b4d59b5c0ba418b0367e4d9e7f864075b7f0cd
izarubin <izarubin@neoflex.ru>
2022-09-19 15:36:12 +0300

    init commit

---
+++ src/main/java/javainternalcourse/convyerMS/ConvyerMsApplication.java
@@ -0,0 +1,13 @@
+package javainternalcourse.convyerMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvyerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvyerMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javainternalcourse/convyerMS/ConvyerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package javainternalcourse.convyerMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvyerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
